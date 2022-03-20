1020f367dc959a26cfa817ae1a95aaf04eae6cce
Luis <seplusi_arcanjo@hotmail.com>
2022-03-20 17:57:37 +0100

    Added documents

--- main/lib/appium_driver.py
+++ main/lib/appium_driver.py
@@ -4,6 +4,4 @@ from appium import webdriver
 class AppiumDriver(object):
     def __init__(self, session):
         desired_caps = session['desired_caps']
-#        desired_caps['app'] = f'{session["proj_path"]}/{desired_caps["app"]}'
-
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver = webdriver.Remote(f'http://localhost:{session["appium_port"]}/wd/hub', desired_caps)
